40c3a128590f051e1749dca2a19a574c0ce00eed
phisayur <fisayoola01@gmail.com>
2023-10-25 07:27:59 -0700

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private attribute size
+"""
+
 
 class Square:
-    def __init__(self, size):
-    
-        Args:
-            size (int): The size of the new square.
+    """
+    class Square definition
 
-            self.__size =size
+    Args:
+        size : size of a side in square
+    """
+    def __init__(self, size):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of a side of square
+        """
+        self.__size = size
